bbe349bb3d3f8b037f5e863da9fa22051a7d9f41
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2019-10-24 10:54:16 +0200

    Functionality to filter bikes from received answers from the bike selector form

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ const bikeForm = document.forms.bikeSelector;
 
 let bikeGender, bikeUsage, bikePlace, bikeStyle;
 
+let findProduct;
+
 function collectForm() {
     bikeGender = bikeForm.querySelector('input[name="bike-gender"]:checked').value;
     bikeUsage = bikeForm.querySelector('input[name="bike-usage"]:checked').value;
@@ -12,8 +14,30 @@ function collectForm() {
 
 function createSection() {
     const section = document.createElement("section");
-    section.setAttribute("id", "selected-bikes")
-    document.querySelector("#bike-selector-content").appendChild(section)
+    const sectionWrapper = document.createElement("div");
+    section.setAttribute("class", "product-section")
+    sectionWrapper.setAttribute("id", "selected-bikes");
+    sectionWrapper.setAttribute("class", "product-section__wrapper");
+    document.querySelector("#bike-selector-content").appendChild(section);
+    section.appendChild(sectionWrapper);
+}
+
+function findBikes() {
+
+    fetch(url)
+        .then((resp) => resp.json()) // Transform the responsed data into json
+        .then(function (data) {
+            let articles = data.products;
+            findProduct = articles.filter(product => { return product.gender === bikeGender && product.usage === bikeUsage });
+            return document.querySelector("#selected-bikes").innerHTML = findProduct.map(articleTemplate).join("");
+            console.log(findProduct);
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
 }
 
 
@@ -21,6 +45,7 @@ bikeForm.addEventListener("submit", function (e) {
     e.preventDefault();
     collectForm();
     createSection();
+    findBikes();
 
 })
 
