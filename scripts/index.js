201186f0ebfbfbd8d3e08bbde2926252535a4385
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2019-11-03 17:20:42 +0100

    Added comments for explanations

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
+// Get necessary HTML elements
 const bikeForm = document.forms.bikeSelector;
 const heading = document.querySelector("#bike-selector-heading");
+const subHeading = document.querySelector(".bike-selector-header__paragraph")
+const startBtn = document.querySelector("#btnStart")
 
-
+// Create variables 
 let bikeGender, bikeUsage, bikePlace, bikeStyle;
 
 let findProduct;
 
+// Collect data from the bike selector form
 function collectForm() {
     bikeGender = bikeForm.querySelector('input[name="bike-gender"]:checked').value;
     bikeUsage = bikeForm.querySelector('input[name="bike-usage"]:checked').value;
@@ -14,6 +18,7 @@ function collectForm() {
     console.log(bikeGender, bikeUsage, bikePlace, bikeStyle)
 }
 
+// Create the section where products is to be shon
 function createSection() {
     const section = document.createElement("section");
     const sectionWrapper = document.createElement("div");
@@ -24,6 +29,26 @@ function createSection() {
     section.appendChild(sectionWrapper);
 }
 
+// Change the appeareance of the header and text based on results 
+function changeHeader(results) {
+
+    document.querySelector(".bike-selector-header").style.height = "50vh";
+    startBtn.remove();
+    subHeading.classList.add("heading--bg")
+
+    if (results.length == 1) {
+        heading.textContent = "Here is your perfect match!"
+        subHeading.textContent = "Your lucky, we found one that suits your needs!"
+    } else if (results.length >= 2) {
+        heading.textContent = "You got some to choose from!"
+        subHeading.textContent = "Your lucky, we found several that suits your needs! Pick and choose."
+    } else {
+        heading.textContent = "We're so sorry, no matches!"
+        subHeading.textContent = "Don't be sad though, contact us and we'll help you find one."
+    }
+}
+
+// Fetch the products from url and filter them based on results from the form
 function findBikes() {
 
     fetch(url)
@@ -31,11 +56,9 @@ function findBikes() {
         .then(function (data) {
             let articles = data.products;
             findProduct = articles.filter(product => { return product.gender === bikeGender && product.usage === bikeUsage && product.type === bikeStyle && product.usagePlace === bikePlace });
-            if (findProduct.length == 1) {
-                heading.textContent = "Here is your perfect match!"
-            } else if (findProduct.length >= 2) {
-                heading.textContent = "Several matches, pick and choose!"
-            } else { heading.textContent = "We're so sorry, no matches!" }
+            // Use the findproduct as argument to find the proper text in changeHeader
+            changeHeader(findProduct);
+            // Return the filtered products to the newly created section
             return document.querySelector("#selected-bikes").innerHTML = findProduct.map(articleTemplate).join("");
         })
 
@@ -46,10 +69,11 @@ function findBikes() {
 
 bikeForm.addEventListener("submit", function (e) {
     e.preventDefault();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     collectForm();
     document.querySelector("#bike-selector-content").innerHTML = "";
     createSection();
     findBikes();
-
 })
 
